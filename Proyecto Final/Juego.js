587b647eee1e207cfae875dd0356526d9c5c76df
IanLinares <115584918+IanLinares@users.noreply.github.com>
2023-02-28 23:30:07 -0300

    Add files via upload

---
+++ Proyecto Final/Juego.js
@@ -0,0 +1,1 @@
+//una de las cosa que no pude realizar fue la implementacion del javascript con los botones, mi idea original consistia en que los botones mostraran por pantalla la informacion en forma de lista */ 
